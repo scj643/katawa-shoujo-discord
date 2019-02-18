4c46d93f935e9295a0015e65484ff155c66a030f
Charles Surett <surettcharles@gmail.com>
2019-02-18 18:53:54 -0500

    Added multiprocessing support

--- katawa.py
+++ katawa.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process, Pipe
 from pypresence import Presence  # The simple rich presence client in pypresence
 import time
 
@@ -8,33 +9,44 @@ route_dict = {'hanako': {'details': "Hanako's Route", 'image': "hanako"},
               'emi': {'details': "Emi's Route", 'image': "emi"},
               'kenji': {'details': "Kenji's Route?", 'image': "kenji"}}
 
-current_route = "hanako"
 
-large_icon = 'main_icon'
+def rpc_process(conn, routes):
+    large_icon = 'main_icon'
 
-client_id = "545357282880389141"  # Put your Client ID in here
-
-if __name__ == '__main__':
-    print("Available routes")
-    for i in route_dict.keys():
-        print(i)
-    start_time = int(time.time())
-    RPC = Presence(client_id)  # Initialize the Presence client
+    client_id = "545357282880389141"  # Put your Client ID in here
+    rpc = Presence(client_id)  # Initialize the Presence client
+    current_route = "hanako"
     connected = False
     while not connected:
         try:
-            RPC.connect()  # Start the handshake loop
+            rpc.connect()  # Start the handshake loop
             connected = True
         except FileNotFoundError:
             connected = False
             print("Is Discord running?")
             time.sleep(5)
+    start_time = int(time.time())
 
-    RPC.update(start=start_time, state="In Game")  # Set's start time
+    rpc.update(start=start_time, state="In Game")  # Set's start time
+
     while True:  # The presence will stay on as long as the program is running
-        new_route = input('new route: ')
-        if new_route in route_dict.keys():
-            route = route_dict[new_route]
-            RPC.update(details=route['details'], large_image=route['image'], small_image=large_icon,
-                       large_text=route['details'], start=start_time, state="In Game")
-        time.sleep(15)  # Can only update rich presence every 15 seconds
+        if current_route in routes.keys():
+            rpc_route = routes[current_route]
+            rpc.update(details=rpc_route['details'], large_image=rpc_route['image'], small_image=large_icon,
+                       large_text=rpc_route['details'], start=start_time, state="In Game")
+        time.sleep(5)  # Can only update rich presence every 15 seconds
+        received = conn.recv()
+        if received:
+            current_route = received
+
+
+if __name__ == '__main__':
+    parent_conn, child_conn = Pipe(duplex=False)
+    p = Process(target=rpc_process, args=(parent_conn, route_dict,))
+    p.start()
+    print("Available routes")
+    for i in route_dict.keys():
+        print(i)
+    while True:
+        route = input("New Route: ")
+        child_conn.send(route)
